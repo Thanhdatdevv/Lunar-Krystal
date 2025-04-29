3febc53fddcd7404bd35d779b745b49b882295b7
Thanhdatdevv <thanhdate09@gmail.com>
2025-04-29 20:18:19 +0700

    Update batgiam.js

--- modules/commands/batgiam.js
+++ modules/commands/batgiam.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "batgiam",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "ProCoderMew",
+    credits: "Pro",
     description: "",
     commandCategory: "Tạo ảnh",
     usages: "[tag]",
