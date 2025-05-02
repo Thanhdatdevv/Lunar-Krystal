eb17412084abb0fcfa0b718186b64a927bb7340d
Thanhdatdevv <thanhdate09@gmail.com>
2025-05-02 12:11:55 +0700

    Update ad.js

--- modules/commands/ad.js
+++ modules/commands/ad.js
@@ -26,3 +26,36 @@ module.exports = {
     const mentionMatch = mentions && Object.keys(mentions).includes(ADMIN_UID);
 
     if (keywordMatch || mentionMatch) {
+      const nameSender = await Users.getNameUser(senderID);
+
+      const fancyText = 
+`𝔅ạ𝔫 𝔫𝔥ắ𝔠 𝔤ì 𝔠𝔥ủ 𝔠ủ𝔞 𝔪ì𝔫𝔥 đấ𝔲 ${nameSender}😆✨
+𝕏𝕚𝕟 𝕔𝕙𝕒̀𝕠 𝕕𝕖̂̂𝕪 𝕝𝕒̀ 𝕔𝕙𝕦̉ 𝕔𝕦̉𝕒 𝕞𝕚̀𝕟𝕙😘
+𝕋𝕖̂𝕟 : 𝕃𝕖̂ 𝕋𝕙𝕒̀𝕟𝕙 𝔻𝕒̣𝕥
+ℕ𝕒̆𝕞 𝕤𝕚𝕟𝕙 : 𝟚𝟘𝟘𝟞
+ℂ𝕦𝕟𝕘 : 𝕔𝕦𝕟𝕘 𝕜𝕙𝕦̉`;
+
+      const videos = [
+        "https://i.imgur.com/7OQBaMo.mp4",
+        "https://i.imgur.com/ruOTcU1.mp4",
+        "https://i.imgur.com/TwGBUTa.mp4",
+        "https://i.imgur.com/mRWp5j2.mp4"
+      ];
+
+      const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+
+      api.sendMessage(
+        {
+          body: fancyText,
+          attachment: [await api.getStreamFromURL(randomVideo)]
+        },
+        threadID,
+        messageID
+      );
+    }
+  },
+
+  run: async function () {
+    // Không dùng lệnh gọi trực tiếp
+  }
+};
